a4ad0aa78d03b0858dd5cf1e5df0b91cab565207
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-09-26 16:01:24 +0430

    Exam and some files added

---
+++ Administrator/Dari/_Partial Components/Navigation.php
@@ -0,0 +1,93 @@
+
+<nav class="nav-admin-panel" id = "nav-admin-panel">
+    <div class="Accordion-container">
+        <div class="dashbord">
+            <a href="index.php"> صفحه اصلی  </a>
+        </div>
+        <div class="dashbord">
+            <a href="Quiz-Result.php">  نتیجه امتحانات </a>
+        </div>
+        <div class="set">
+            <a href="#"> <i class="fa fa-angle-left"> </i> سوالات  </a>
+        
+            <div class="content">
+                <a href="Add-Questions.php" class="nav-link ">       
+                    <li class="nav-item">
+                        درج کردن سوالات
+                    </li>
+                </a>
+                <a href="Manage-Questions.php" class="nav-link ">
+                    <li class="nav-item">
+                        مدیریت سوالات
+                    </li>
+                </a>
+            </div>
+        </div>
+        <div class="set">
+            <a href="#">  مضامین <i class="fa fa-angle-left"> </i> </a>
+        
+            <div class="content">
+                <a href="Add-Subject.php" class="nav-link ">       
+                    <li class="nav-item">
+                          درج کردن مضامین
+                    </li>
+                </a>
+                <a href="Manage-Subject.php" class="nav-link ">
+                    <li class="nav-item">
+                            مدیریت مضامین
+                    </li>
+                </a>
+            </div>
+        </div>
+        <div class="set">
+            <a href="#"> پوهنځي ها <i class="fa fa-angle-left"> </i> </a>
+        
+            <div class="content">
+                <a href="Add-Faculty.php" class="nav-link ">       
+                    <li class="nav-item">
+                          درج کردن پوهنځی
+                    </li>
+                </a>
+                <a href="Manage-Faculty.php" class="nav-link ">
+                    <li class="nav-item">
+                            مدیریت پوهنڅی
+                    </li>
+                </a>
+            </div>
+        </div>
+        <div class="set">
+            <a href="#"> اساتید <i class="fa fa-angle-left"> </i> </a>
+        
+            <div class="content">
+                <a href="Add-Teacher.php" class="nav-link ">       
+                    <li class="nav-item">
+                          درج کردن اساتید
+                    </li>
+                </a>
+                <a href="Manage-Teacher.php" class="nav-link ">
+                    <li class="nav-item">
+                            مدیریت اساتید
+                    </li>
+                </a>
+            </div>
+        </div>
+        <div class="set">
+            <a href="#"> یوزر ها<i class="fa fa-angle-left"> </i> </a>
+        
+            <div class="content">
+                <a href="Add-Users.php" class="nav-link ">       
+                    <li class="nav-item">
+                          درج کردن یوزرها
+                    </li>
+                </a>
+                <a href="Manage-Users.php" class="nav-link ">
+                    <li class="nav-item">
+                            مدیریت یوزر ها
+                    </li>
+                </a>
+            </div>
+        </div>
+    </div>
+</nav>
+
+
