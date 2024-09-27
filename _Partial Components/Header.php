1a3bad4b3cecc91ab90322853d1cda0d245acc6f
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-09-27 16:39:39 +0430

    Header Bootstrap links modified

--- _Partial Components/Header.php
+++ _Partial Components/Header.php
@@ -44,9 +44,9 @@ $exm = new Exam();
     <link href="CSS/animated.css" rel="stylesheet" >
     <!-- <link href="./CSS/bootstrap.min.css" rel="stylesheet"> -->
       
-    <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <!-- Bootstrap -->
-    <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script> -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
     <script src="js/bootstrap.min.js"></script>
     <script src="./js/bootstrap.js"></script>
@@ -222,7 +222,7 @@ $exm = new Exam();
       </div>
     </nav>
     <body>
-<script src = "./jquery.min.js"></script>
+<!-- <script src = "./jquery.min.js"></script>
 <script src = "./../js/collapse.js"></script>
 <script src = "./../js/transition.js"></script>
 <script src = "./../js/modal.js"></script>
@@ -231,3 +231,5 @@ $exm = new Exam();
 <script src = "./../js/dropdown.js"></script>
 <script src = "./../js/carousel.js"></script> 
 <script src = "./js/OnlineQuiz.js"></script> 
+<script src = "./js/jquery.js"></script>
+<script src = "./js/bootstrap.min.js"></script> -->
