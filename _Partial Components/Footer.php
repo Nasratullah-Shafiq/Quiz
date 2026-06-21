3ab18056cf355be853a1e6b67b4259fd7354f58e
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2026-06-21 08:40:32 +0430

    Refactor Footer.php: Organize and streamline script inclusions for improved maintainability

--- _Partial Components/Footer.php
+++ _Partial Components/Footer.php
@@ -2,7 +2,20 @@
             <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
             <!-- Include all compiled plugins (below), or include individual files as needed -->
-            
+            <script src="./js/bootstrap.min.js"></script>
+            <script src="./assets/js/tests/vendor/jquery.min.js"></script>
+            <script src="./assets/js/transition.js"></script>
+            <script src="./assets/js/tooltip.js"></script>
+            <script src="./assets/js/affix.js"></script>
+            <script src="./assets/js/alert.js"></script>
+            <script src="./assets/js/button.js"></script>
+            <script src="./assets/js/carousel.js"></script>
+            <script src="./assets/js/collapse.js"></script>
+            <script src="./assets/js/dropdown.js"></script>
+            <script src="./assets/js/modal.js"></script>
+            <script src="./assets/js/scrollspy.js"></script>
+            <script src="./assets/js/tab.js"></script>
+            <script src="./assets/js/bootstrap.bundle.js"></script>
         <footer class ="footer">
             <div class="container">
 
@@ -11,11 +24,9 @@
                 <a style="color: rgb(0,176,240)" href="http://www.salam.edu.af">
                     Nasratullah Shafiq
                 </a>.
-
+                
                 <?= $lang['footer_text']; ?>
-
                 <?php echo date('Y'); ?>.
-
             </div>
         </footer>
     </body>   
